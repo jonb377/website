1de19451afea64e181c69a467aabe8104c0d978a
Jon Bolin <jonb377@gmail.com>
2019-07-25 21:20:04 -0700

    Initialize auth service

---
+++ auth-service/auth/auth.go
@@ -0,0 +1,44 @@
+package auth
+
+import (
+    userProto "github.com/jonb377/website/user-service/proto/user"
+    pb "github.com/jonb377/website/auth-service/proto/auth"
+    "fmt"
+    "github.com/micro/go-micro"
+    "github.com/micro/go-micro/client"
+    "log"
+)
+
+func RunAuthService() {
+    db, err := CreateConnection()
+
+    if err != nil {
+        log.Fatalf("Could not connect to DB: %v", err)
+    }
+
+    defer db.Close()
+
+
+    db.AutoMigrate(&Session{})
+
+    srv := micro.NewService(
+        micro.Name("go.micro.api.auth"),
+        micro.Version("latest"),
+    )
+
+    srv.Init()
+
+
+
+    if err := pb.RegisterAuthServiceHandler(srv.Server(), &AuthService{
+        db: db,
+        userClient: userProto.NewUserService("go.micro.api.user", client.DefaultClient),
+        tokenService: &TokenService{},
+    }); err != nil {
+        fmt.Println(err)
+    }
+
+    if err := srv.Run(); err != nil {
+        fmt.Println(err)
+    }
+}
--- auth-service/main.go
+++ auth-service/main.go
@@ -1,46 +1,10 @@
 package main
 
 import (
-    userProto "github.com/jonb377/website/user-service/proto/user"
-    pb "github.com/jonb377/website/auth-service/proto/auth"
     auth "github.com/jonb377/website/auth-service/auth"
-    "fmt"
-    "github.com/micro/go-micro"
-    "github.com/micro/go-micro/client"
-    "log"
 )
 
 func main() {
-    db, err := auth.CreateConnection()
-
-    if err != nil {
-        log.Fatalf("Could not connect to DB: %v", err)
-    }
-
-    defer db.Close()
-
-
-    db.AutoMigrate(&auth.Session{})
-
-    srv := micro.NewService(
-        micro.Name("go.micro.api.auth"),
-        micro.Version("latest"),
-    )
-
-    srv.Init()
-
-
-
-    if err := pb.RegisterAuthServiceHandler(srv.Server(), &auth.AuthService{
-        db: db,
-        userClient: userProto.NewUserService("go.micro.api.user", client.DefaultClient),
-        tokenService: &auth.TokenService{},
-    }); err != nil {
-        fmt.Println(err)
-    }
-
-    if err := srv.Run(); err != nil {
-        fmt.Println(err)
-    }
+    auth.RunAuthService()
 }
 
